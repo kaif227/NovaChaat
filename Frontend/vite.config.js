20dcba108868d0d14819c01a7310bf44e00c1d95
kaif <kaifbhati58@gmail.com>
2025-10-19 15:04:53 +0530

    correction in vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,16 +1,9 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   base: "./",
-// })
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "./", // 👈 critical for proper asset paths after deployment
   test: {
     globals: true,
     environment: "jsdom",
