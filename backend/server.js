11684c0c4b9ebd66a6f91edee6924c22c2019055
kaif <kaifbhati58@gmail.com>
2026-06-05 12:59:06 +0530

    changes in chat.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-import chatRouter from './Routes/Chat.js';
+import chatRouter from './routes/chat.js';
 
 const app = express();
 const PORT = 8080;
